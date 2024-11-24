0b7afad89cf92de4e1cff6f99b4adb6a0e9d6b06
Dikshant <dikshantahalawat2005@gmail.com>
2024-11-24 16:04:59 +0530

    category screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ott_app/loginservices/login.dart';
+import 'package:ott_app/screens/homescreen/categoreis.dart';
 
 void main() {
  runApp(const ProviderScope(child: MyApp()));}
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue, 
       ),
-      home: const LoginPage(), 
+      home: const Categoreis(), 
     );
   }
 }
